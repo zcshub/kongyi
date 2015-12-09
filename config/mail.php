baea473a0100862feb03d6103f85eeedd1488735
zczhang <994125301@qq.com>
2015-12-09 19:38:10 +0800

    config mail

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'email@kongyi.xyz', 'name' => 'kongyi.xyz'],
 
     /*
     |--------------------------------------------------------------------------
