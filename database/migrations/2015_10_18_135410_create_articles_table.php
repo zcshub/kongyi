2be44828a1dc75e98542a529ef949ea9eddbd91b
zczhang <994125301@qq.com>
2016-01-29 19:24:58 +0800

    use text for article content

--- database/migrations/2015_10_18_135410_create_articles_table.php
+++ database/migrations/2015_10_18_135410_create_articles_table.php
@@ -16,8 +16,8 @@ class CreateArticlesTable extends Migration
             $table->increments('id');
             $table->timestamp('publish_at');
             $table->string('title');
-            $table->string('content');
-            $table->string('html_content');
+            $table->text('content');
+            $table->text('html_content');
             $table->string('tag');
             $table->timestamps();
         });
