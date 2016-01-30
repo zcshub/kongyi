e34bdd04b5e98cda9a07feb657d1af92b3688e1e
zczhang <994125301@qq.com>
2016-01-31 00:52:43 +0800

    add a target:_blank in Markdown article

--- resources/views/article/article.blade.php
+++ resources/views/article/article.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 
+<script>
+	$(document).ready(function() {
+	    //为超链接加上target='_blank'属性
+		$('a[href^="http"]').each(function() {
+			$(this).attr('target', '_blank');
+		});
+	});
+</script>
+
 <div class="col-md-8 col-md-offset-2">
 	<div class="page-header">
 		<h1>{{ $article->title }}</h1>
