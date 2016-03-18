38d5cf4dfe632f51da64f468754819385c960401
zczhang <994125301@qq.com>
2016-03-18 22:49:12 +0800

    add baidu push

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,13 @@
 $(function () {
+	/*ajax token配置*/
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
 	});
+	/*baidu 站长推送*/
+    var bp = document.createElement('script');
+    bp.src = '//push.zhanzhang.baidu.com/push.js';
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
 });
