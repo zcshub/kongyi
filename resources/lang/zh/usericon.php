81ba68ebd2a66dd68664c6d3f95e9566dec54327
zczhang <994125301@qq.com>
2016-03-18 18:10:11 +0800

    add icon

--- resources/lang/zh/usericon.php
+++ resources/lang/zh/usericon.php
@@ -3,5 +3,21 @@
 return [
 	'default.jpg',
 	'acniang.jpg',
-	'wukong.jpg'
+	'wukong.jpg',
+	's1.jpg',
+	's2.jpg',
+	's3.jpg',
+	's4.jpg',
+	's5.jpg',
+	's6.jpg',
+	's7.jpg',
+	's8.jpg',
+	's9.jpg',
+	's10.jpg',
+	's11.jpg',
+	's12.jpg',
+	's13.jpg',
+	's14.jpg',
+	's15.jpg',
+	's16.jpg',
 ];
