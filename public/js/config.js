70085d732d56e3a09b7be5f000c0c4d9e03a0cde
zczhang <994125301@qq.com>
2016-04-22 14:26:10 +0800

    取消百度推送

--- public/js/config.js
+++ public/js/config.js
@@ -5,9 +5,4 @@ $(function () {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
 	});
-	/*baidu 站长推送*/
-    var bp = document.createElement('script');
-    bp.src = '//push.zhanzhang.baidu.com/push.js';
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(bp, s);
 });
